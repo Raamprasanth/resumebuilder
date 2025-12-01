db1c32ea882a1dd248ccf9633d3bb8fefd7909bf
Bucky # <bucky02062005@gmail.com>
2025-12-01 16:49:48 +0000

    Tore env file // Import the functions you need from the SDKs you need im

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,10 +1,9 @@
 export const firebaseConfig = {
-  apiKey: "AIzaSyBlVgAnT8xackiJtE7fLopKBQOkehnFbf8",
-  authDomain: "vexman-9b85e.firebaseapp.com",
-  projectId: "vexman-9b85e",
-  storageBucket: "vexman-9b85e.firebasestorage.app",
-  messagingSenderId: "887523461668",
-  appId: "1:887523461668:web:3c499653882b14337fa6b1",
-  measurementId: "G-C81F1522RT"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
-    
