6faaaba58894ee5a1317fb9b1c120ff48e6596f0
Bucky # <bucky02062005@gmail.com>
2025-12-01 19:35:25 +0000

    Make a logo like this

--- src/components/ui/logo.tsx
+++ src/components/ui/logo.tsx
@@ -4,13 +4,14 @@ export function Logo({ className, ...props }: React.SVGProps<SVGSVGElement>) {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 258 206"
+      viewBox="0 0 160 160"
       className={cn("h-8 w-8", className)}
       {...props}
     >
       <path
         fill="currentColor"
-        d="M53.33.22L.54,58.04l53.8,42.59,85.25-6.72L53.33.22z M104.53,84.14L50.4,127.2l103.1,8.12,54.12-42.5-103.1-8.68z M153.51,147.28L99.2,190.22l158.26,15.53.3-64.1-104.25,3.63z"
+        d="M100,20 L150,20 L150,50 L100,50 Z M50,70 L100,70 L100,100 L50,100 Z M0,120 L50,120 L50,150 L0,150 Z"
+        transform="skewY(-30) translate(20, -10)"
       />
     </svg>
   );
