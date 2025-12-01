0f73c0731466bc9ea7c51fe142119fcc4ab17c65
Bucky # <bucky02062005@gmail.com>
2025-12-01 18:02:00 +0000

    Try fixing this error: `Runtime Error: Error: DropdownMenuSeparator is n

--- src/components/layout/app-sidebar.tsx
+++ src/components/layout/app-sidebar.tsx
@@ -34,6 +34,7 @@ import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
+  DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu';
 import { Button } from '../ui/button';
