b6709bbac4eac9d0c8e137fd183c401dd9758554
Raamprasanth <raam3124@gmail.com>
2026-06-15 15:10:09 +0530

    fix: refactor Logo to use standard img tag

--- src/components/ui/logo.tsx
+++ src/components/ui/logo.tsx
@@ -1,15 +1,11 @@
-import Image from "next/image";
 import { cn } from "@/lib/utils";
 
-export function Logo({ className, ...props }: Partial<React.ComponentProps<typeof Image>>) {
+export function Logo({ className, ...props }: React.ImgHTMLAttributes<HTMLImageElement>) {
   return (
-    <Image
+    <img
       src="/logo.png"
       alt="Job Genie Logo"
-      width={200}
-      height={80}
       className={cn("object-contain", className)}
-      priority
       {...props}
     />
   );
